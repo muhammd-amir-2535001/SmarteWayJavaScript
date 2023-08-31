618c346779a9897b1e874f5979743d4dad0d43c1
Muhammad Amir <126454691+muhammd-amir-2535001@users.noreply.github.com>
2023-08-31 16:31:00 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,408 @@
+/* An alert is a box that pops up to give the 
+user a message heres code for an alert that displays
+the "thanks for your input"
+alert is a keyword 
+the quoted text "Thanks for your input" is called a 
+text string or simply a string*
+
+alert("lalkhan i am coming");
+alert("amir");
+/* console.log is function that write message on
+console.log/terminal also when working with server side
+javascript we can use console.log for logging and
+and out put will be in terminal 
+
+console.log("muhammmad amir");
+console.log("10+10");
+ for testing purpose you can use document
+ .write todisplay message or text in browser
+ window this will be displayed in browser window
+
+document.write("hi lalkhan kasy hu");
+document.write(3+3);
+// variables are contaniners for storing data
+values and variables are used to store information to be
+referenced and manpulated in a comuter program
+variable is a term that refers to a particular
+value
+
+var age = 24;
+var nationality = "pakistan";
+var feepaid = true;
+var f = 2.5;
+alert(age);
+alert(nationality);
+alert(feepaid);
+alert(f);*/
+
+/*      DATA TYPES
+variables containsn data 
+Each data has types thats why we call it data
+type
+variables can hold numbers like 100 and text
+values like "muhammad amir"
+in programming text values are called text string
+six types are considered to be primitives
+a.NUMBER = inergers , floats etc
+b.string = an array of characters
+c.BOOLEAN = true or false
+d.NULL = no value
+e.UNDEFINED = a declared variable but hasn't been given 
+  a value
+f.SYMBOL = a unique value thats not equal to any other 
+           value
+2. COMPLEX TYPES
+       A.objects
+       B.function
+funtion is callable object that executes a block
+of code it lies under the object type
+// STRING VARIABLE
+var name = "lalkhan";
+alert(name);
+name = "muhammad amir";
+alert(name);
+// NUMBERS VARIABLE
+var age = 25;
+var weihght = 12.5;
+alert(age)
+age = 25 + 5;
+alert(age);
+// VARIABLE FOR BOOLEAN
+// to store true or false boolean data type
+var isFeePaid = true;
+var exampassed = false;
+alert(isFeePaid);
+alert(exampassed);
+
+// FOR UNDEFINED
+var n ;
+var age ;
+alert(n);
+
+// FOR NULL
+var name = null;
+var nationality = "pakistan";
+alert(nationality);
+   nationality = null;
+   alert(name);
+   alert(nationality);
+   document.write(nationality);
+*/
+///arithmetic operators
+// var a = 5;
+// var b = 5;
+// var c = a + b;
+// var d = a - b;
+// var e = a / b;
+// var f = a * b;
+// var g = a ** b;
+// var h = a % b;
+// console.log(c);
+// console.log(d);
+// console.log(e);
+// console.log(f);
+// console.log(g);
+// console.log(h);
+//ASSIGNMENT OPERATORS
+// var a =  5;
+// a+=5;
+// console.log(a);
+// a-=5;
+// console.log(a);
+// a*=5;
+// console.log(a);
+// a/=5;
+// console.log(a);
+// a**=5;
+// console.log(a);
+// a%=5;
+// console.log(a);
+//increment and decrement operator
+// var a = 12;
+// console.log(a);
+// a++;
+// console.log(a);
+// a--;
+// console.log(a);
+// ++a;
+// console.log(a)
+// --a;
+// console.log(a)
+//CONCATENATING TEXT STRINGS
+// var fName = "Muhammad";
+// var sNamr = "Amir";
+// console.log(fName+" "+sNamr);
+// var q = "3";
+// var w = 5
+// console.log(q+w);
+
+//JavaScript Programming Assignment 1
+//Note: Check Rules end of document
+//1. Write a script to display the following message on your web page:
+//(Hint : Use line break)
+
+//alert("Welcome to JS land...<br>");
+//alert("Happy Coding!.");
+//2. Write a script to save student’s bio data in JS variables and show the
+//data in alert boxes.
+// var firstName = "Muhammad";
+// var lastName ="Amir";
+// var age = 25;
+// var address = "gigit conony karongi karachi"
+// var phone_Nunber = "03152535001";
+// var email = "lalkhan2535 "
+// var  CNIC_NO = "1242-2452255-8";
+// var education = "BS software engineering";
+// alert("Name:"+" "+firstName+" "+lastName);
+// alert("Age:"+" "+age);
+// alert("Address:"+" "+address);
+// alert("PhoneNamber:"+" "+phone_Nunber);
+// alert("Email:"+" "+email);
+// alert("CNIC NO:" +" "+CNIC_NO);
+// alert("Education:"+""+education);
+// alert("THANKS YOU:");
+// 7. Display this in your browser
+// a. A heading stating “Rules for naming JS variables”
+// b. Variable names can only contain ______, ______,
+// c. ______ and ______.
+// d. For example $my_1stVariable.
+// e. Variables must begin with a ______, ______ or
+// f. _____. For example $name, _name or name
+// g. Variable names are case _________
+// h. Variable names should not be JS ________
+
+// document.write("<h1>Rules for naming JS variables</h1>");
+// document.write("<p>Variable names can only contain letters, numbers, underscores, and dollar signs.</p>");
+// document.write("<p>For example $my_1stVariable.</p>");
+// document.write("<p>Variables must begin with a letter, underscore or dollar sign.</p>");
+// document.write("<p>For example $name, _name or name</p>");
+// document.write("<p>Variable names are case sensitive.</p>");
+// document.write("<p>Variable names should not be JS keywords.</p>");
+
+//8. Write a program that takes two numbers & add them in a new
+//variable. Show the result in your browser.
+/*<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script src="./index.js"></script>
+    <title>Display Message</title>
+</head>
+<body>
+<h2>JavaScript Variables</h2>
+<p>Click the button to add two
+ numbers and display the result.</p>
+<button onclick="myFunction()">Try it</button>
+<p id="demo"></p>
+<script>
+function myFunction() {
+  var x = 5;
+  var y = 3;
+  var z = x + y;
+  document.getElementById("demo").innerHTML = "The sum of " + x + " and " + y + " is: " + z;
+}
+</script>
+</body>
+</html>*/
+/*10. Do the following using JS Mathematic Expressions
+a. Declare a variable.
+b. Show the value of variable in your browser like “Value after
+variable declaration is: ??”.
+c. Initialize the variable with some number.
+d. Show the value of variable in your browser like “Initial value: 5”.
+e. Increment the variable.
+f. Show the value of variable in your browser like “Value after
+increment is: 6”.
+g. Add 7 to the variable.
+h. Show the value of variable in your browser like “Value after
+addition is: 13”.
+i. Decrement the variable.
+j. Show the value of variable in your browser like “Value after
+decrement is: 12”.
+k. Show the remainder after dividing the variable’s value by 3.
+l. Output : “The remainder is : 0”.*/
+/*<body>
+    <h2>JavaScript Variables</h2>
+<p>Click the button to see the result.</p>
+<button onclick="myFunction()">Try it</button>
+<p id="demo"></p>
+<script>
+function myFunction() {
+  var x;
+  document.getElementById("demo").innerHTML = "Value after variable declaration is: " + x;
+  x = 5;
+  document.getElementById("demo").innerHTML += "<br>Initial value: " + x;
+  x++;
+  document.getElementById("demo").innerHTML += "<br>Value after increment is: " + x;
+  x += 7;
+  document.getElementById("demo").innerHTML += "<br>Value after addition is: " + x;
+  x--;
+  document.getElementById("demo").innerHTML += "<br>Value after decrement is: " + x;
+  document.getElementById("demo").innerHTML += "<br>The remainder is: " + (x % 3);
+}
+</script>
+</body>*/
+
+//promt() funtion
+//prompt("Enter you name");
+// var question = "when software engineering is done?:";
+// var age = prompt(question);
+// var newage = age + 5;
+// console.log("will done =" + newage);
+   // CONVERT STRING TO INTEGER
+          //parseInt
+  //  var age = prompt("what is your age?");
+  //  var num = 4;
+  //  var sum = parseInt(age)+num;
+  //  console.log(sum);
+       //parseFloat()funtion
+  // var age = prompt("what is your age");
+  // var num = 2;
+  // var sum = parseFloat(age)+num;
+  // document.write(sum);
+     //NUMBER FUNCTION
+    //  var age = prompt("what is your age?");
+    //  var num = 4;
+    //  var sum  = num + Number(age);
+    //  document.write(sum);
+    //var a = Number("rffydyrd");
+    //document.write(a);
+    // var a = 3;
+    // console.log(a==3);
+    // console.log(a=="3");
+    // console.log(a==="3");
+    // this is && operator
+    // var a = 60;
+    // var b = a>50 && a<70;
+    // console.log(b);
+    // var x = 80;
+    // var y = x>50 && x<70;
+    // console.log(y);
+    //this is || (or)operator
+    //  var a = 60;
+    //  var b = a<50 || a<50;
+    //  document.write(b);
+    // var c = 80;
+    // var b = c<50 || c>70;
+    // document.write(b)
+    //this is operator os not !
+    //var a = 60;
+    //var b = !(a<50);
+    //alert(b);
+    //var b = !(a>50);
+    //alert(b);
+    //CONDITIONS: IF ELSE
+    /*var age = 15;
+    if(age<18){
+      console.log("you can not enter");
+    }
+    else
+    {
+      console.log("you can enter");
+    }
+*/
+/*var x = parseInt(prompt("Enter a Number", "0"), 10);
+var b = x+0;
+if(b>=90 && b<=100)
+{
+  document.write("you get A1 great");
+}
+else if (b>=80 && b<=90){
+document.write("you get A great");
+}
+else if (b>70 && b<80)
+{
+  document.write("you get B+ great");
+}
+else if (b>=60 && b<70)
+{
+  document.write("you will get B great");
+}
+else if(b>=50 && b<=60)
+{
+document.write("you will get C great");
+}
+else if(b>=40 && b<=50){
+  document.write("you will get D great");
+}else if(b>=35 && b<=40)
+{
+  document.write("you are just pass");
+
+}
+else{
+  document.write("fail");
+}
+*/
+/*var num = parseInt(prompt("wach table you want to print?"));
+for( var i=1; i<=10; i++)
+{
+  console.log(num+"X"+i+"="+(num*i));
+  
+}
+var num = 11;
+for(i=1;i<=10;i++)
+{
+  console.log(num+"X"+i+"="+(num*i));
+}
+for(i=0; i<=10; i++)
+{
+  if(i==4)
+  {
+    break;
+  }
+  document.write("I==;",i);
+}
+for(j=0; j<=10; j++)
+{
+  if(j==4)
+  {
+    continue;
+  }
+  console.log(j);
+}
+
+var a = Math.random();
+console.log("A=",+a);
+var num1 = a*6;
+console.log("nam1="+num1);
+*/
+// var food = ["pizza","burger","snacks"];
+// console.log(food[0]);
+// console.log(food[1]);
+// console.log(food[2]);
+// console.log(food)
+// var food = [];
+// console.log(food);
+// food.push("amir")
+// food.push("ali")
+// food.push("raja")
+// console.log(food);
+// console.log(food[1]);
+// food[1]="lalkhan"
+// console.log(food[1]);
+// var name1 = [];
+// console.log(name1)
+// name1.push("muhammad");
+// name1.push("amir");
+// name1.push("ali");
+// name1.push("raja");
+// console.log(name1);
+// name1.pop()
+// name1.pop()
+// name1.pop()
+// name1.pop()
+
+// console.log(name1);
+// console.log("length of name1"+name1.length);
+var food = [];
+food.push("pizza");
+food.push("burgar");
+food.push("snacks");
+console.log(food);
+console.log("length of food"+food.length);
+var a = food.shift();
+var b = food.shift();
+console.log(food);
+console.log("now length is"+food.length);
